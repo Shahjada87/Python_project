75d4d35438a88d81950a80dbc3737c68a2a5d7a3
SHAHJADA E SAQUALAIN <princesajushahjada0807@gmail.com>
2025-10-08 11:05:21 +0100

    Check a num is odd or even

---
+++ 2.081025.py
@@ -0,0 +1,8 @@
+# check a number is odd or even 
+
+num1 = int(input("Please enter a number = "))
+
+if num1 % 2 == 0:
+    print("The number is an even number.")
+else:
+    print("the number is an odd number.")
